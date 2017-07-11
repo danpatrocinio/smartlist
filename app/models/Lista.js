83db30235c57f912f57f78e7d8e8b05495a12900
danpatrocinio <dpatrocinio@gmail.com>
2017-07-10 22:53:20 -0300

    Models

--- app/models/Lista.js
+++ app/models/Lista.js
@@ -3,10 +3,40 @@ var mongoose = require('mongoose');
 module.exports = function() {
 
 	var schema = mongoose.Schema({
+		usuario: {
+			type: Schema.Types.ObjectId,
+			ref: 'Usuario',
+			required: true
+		},
 		descricao : {
 			type : String,
 			required : true
-		}
+		},
+		itens: [{
+			codigo: Number,
+			descricao: String,
+			quantidade: Number,
+			preco: Number,
+			checked: Boolean
+		}],
+		notificacoes: [{
+			dataHora: {
+            	type: Date
+        	},
+			tipo: {
+				type: String,
+				required: true,
+			},
+			checked: {
+				type: Boolean
+			}
+		}],
+		dataInclusao: {
+			type: Timestamp,
+			default: Date.now
+		},
+		dataChecked: Timestamp,
+		dataRemocao: Timestamp
 	});
 
 	return mongoose.model('Lista', schema);
